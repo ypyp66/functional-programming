29e718a33a155d50a65d1868549735a5a7378bff
kitebeen <ypyp66@naver.com>
2021-12-05 04:02:47 +0900

    함수 만들어보기

--- 1-3.js
+++ 1-3.js
@@ -1,4 +1,5 @@
-const _ = require("./libs");
+//const _ = require("./libs");
+import * as _ from "./libs.js";
 
 let users = [
   { id: 1, name: "ID", age: 32 },
@@ -36,6 +37,15 @@ console.log("123123", _.find(users, bmatch("id", 1)));
 console.log(_.filter(users, bmatch("age", 32)));
 console.log(_.map(users, bmatch("age", 32)));
 
+function bmatch1(key, val) {
+  //한 개의 object에 대한 비교만 가능
+  return function (obj) {
+    return obj[key] === val;
+  };
+}
+
+console.log("bmatch1", _.find(users, bmatch1("id", 1))); //users에서 id가 1인 user를 return
+
 function object(key, val) {
   let obj = {};
   obj[key] = val;
@@ -48,3 +58,31 @@ function match(obj, obj2) {
   }
   return true;
 }
+
+function bmatch2(obj2, val) {
+  console.log("arguments", arguments);
+  if (arguments.length === 2) obj2 = object(obj2, val);
+  return function (obj) {
+    return match(obj, obj2);
+  };
+}
+
+console.log(_.findIndex(users, bmatch2({ name: "JM", age: 32 }))); //5
+console.log(_.findIndex(users, bmatch2({ name: "JM", age: 31 }))); //-1
+//=====================
+console.log(
+  _.filter2([1, 2, 3, 4], function (val, idx) {
+    return idx > 1;
+  })
+);
+
+// identity란?
+let a = 10;
+console.log("identity", _.identity(a));
+
+//predicate로 _.identity를 사용한 경우
+console.log(_.filter2([true, 0, 10, "a", false, null], _.identity)); //[true, 10, 'a'], truthy 값들만 return됨
+console.log(_.some([0, null, 2])); //true
+console.log(_.some([0, null, false])); //false
+console.log(_.every([0, null, 2])); //false
+console.log(_.every([{}, true, 2])); //true
--- libs.js
+++ libs.js
@@ -1,12 +1,21 @@
-exports.filter = function (list, predicate) {
+export function filter(list, predicate) {
   let new_list = [];
   for (let i = 0; i < list.length; i++) {
     if (predicate(list[i])) new_list.push(list[i]);
   }
   return new_list;
-};
+}
 
-exports.map = function map(list, iteratee) {
+export function filter2(list, predicate) {
+  //list : array, predicate : function (조건)
+  let new_list = [];
+  for (let i = 0; i < list.length; i++) {
+    if (predicate(list[i], i, list)) new_list.push(list[i]); //predicate가 true면 new_list에 push
+  }
+  return new_list;
+}
+
+export function map(list, iteratee) {
   /*
     bvalue(key)가 오므로 iteratee는 bvalue()(list[i])가 된다
   */
@@ -16,38 +25,71 @@ exports.map = function map(list, iteratee) {
   }
 
   return new_list;
-};
+}
 
-exports.findById = function (list, id) {
+export function findById(list, id) {
   for (let i = 0; i < list.length; i++) {
     if (list[i].id === id) return list[i];
   }
-};
+}
 
-exports.findByName = function (list, name) {
+export function findByName(list, name) {
   for (let i = 0; i < list.length; i++) {
     if (list[i].name === name) {
       return list[i];
     }
   }
-};
+}
 
-exports.findByAge = function (list, age) {
+export function findByAge(list, age) {
   for (let i = 0; i < list.length; i++) {
     if (list[i].age === age) {
       return list[i];
     }
   }
-};
+}
 
-exports.findBy = function (key, list, val) {
+export function findBy(key, list, val) {
   for (let i = 0; i < list.length; i++) {
     if (list[i][key] === val) return list[i];
   }
-};
+}
 
-exports.find = function (list, predicate) {
+export function find(list, predicate) {
   for (let i = 0; i < list.length; i++) {
-    if (predicate(list[i])) return list[i];
+    if (predicate(list[i])) return list[i]; //predicate()가 true면 해당 요소를 return
   }
-};
+}
+
+export function findIndex(list, predicate) {
+  //list : array, predicate : function
+  for (let i = 0; i < list.length; i++) {
+    if (predicate(list[i])) {
+      return i;
+    }
+  }
+  return -1; //없으면 -1을 리턴
+}
+
+export function identity(v) {
+  return v;
+}
+
+//some, every 만들기
+export function some(list) {
+  //list 값 중 하나라도 true이면 true를 return
+
+  return !!find(list, identity);
+  //!!는 확실한 논리결과를 가지고 오기 위한 연산자
+  //!!true === true, !!undefined === false
+}
+
+export function every(list) {
+  //모든 값이 true이면 true를 return
+  return filter2(list, identity).length === list.length;
+  /*
+  - flow
+  identity()의 값이 true인 값들이 배열로 넘어옴
+  결과 배열의 길이와 주어진 배열의 길이가 같으면 true, 아니면 false
+  */
+}
